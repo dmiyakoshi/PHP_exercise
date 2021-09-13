134c3533bf7c975bbcf048c766c7aaa94ba56366
dmiyakoshi <d1m354dai@gmail.com>
2021-09-13 17:55:10 +0900

    スペースや{}の修正と、if_05.phpの®宣言の位置を修正しました

--- exercise/02/if_5.php
+++ exercise/02/if_5.php
@@ -2,15 +2,14 @@
 
 $num = 73;
 
-$i = 2;
-
-for ($i; $i < $num; $i++) {
+for ($i = 2; $i < $num; $i++) {
     if ($num % $i == 0) {
         echo $num . 'は素数ではありません';
         break;
     }
+
+    if ($i + 1 == $num) {
+        echo $num . 'は素数です';
+    }
 }
 
-if ($i == $num) {
-    echo $num . 'は素数です';
-}
