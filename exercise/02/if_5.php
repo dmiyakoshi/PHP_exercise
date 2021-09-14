7528dc6527a45c39ed44e3242ade1119f12efb2f
dmiyakoshi <d1m354dai@gmail.com>
2021-09-14 11:16:41 +0900

    if_5のfor文の条件の変更と calcのelseifのスペースを統一しました。

--- exercise/02/if_5.php
+++ exercise/02/if_5.php
@@ -2,13 +2,13 @@
 
 $num = 73;
 
-for ($i = 2; $i < $num; $i++) {
+for ($i = 2; $i <= $num; $i++) {
     if ($num % $i == 0) {
         echo $num . 'は素数ではありません';
         break;
     }
 
-    if ($i + 1 == $num) {
+    if ($i == $num) {
         echo $num . 'は素数です';
     }
 }
--- exercise/03/calc.php
+++ exercise/03/calc.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>calc.php</title>
-    </head>
-    <body>
-        <?php 
-            $num1 = $_GET['num1'];
-            $num2 = $_GET['num2'];
 
-            $operation = $_GET['operator'];
+<head>
+    <meta charset="utf-8">
+    <title>calc.php</title>
+</head>
 
-            if ($operation == 'addition'){
-                echo $num1 . ' + '. $num2 . ' = ' .  $num1 + $num2;
-            }elseif ($operation == 'subtraction') {
-                echo $num1 . ' - '. $num2 . ' = ' .  $num1 - $num2;
-            } elseif ($operation == 'multiplication') {
-                echo $num1 . ' * '. $num2 . ' = ' .  $num1 * $num2;
-            } elseif ($operation == 'division') {
-                echo $num1 . ' / '. $num2 . ' = ' .  $num1 / $num2;
-            } else {
-                echo '正しい演算子を指定して下さい';
-            }
-        ?>
-    </body>
+<body>
+    <?php
+    $num1 = $_GET['num1'];
+    $num2 = $_GET['num2'];
+
+    $operation = $_GET['operator'];
+
+    if ($operation == 'addition') {
+        echo $num1 . ' + ' . $num2 . ' = ' .  $num1 + $num2;
+    } elseif ($operation == 'subtraction') {
+        echo $num1 . ' - ' . $num2 . ' = ' .  $num1 - $num2;
+    } elseif ($operation == 'multiplication') {
+        echo $num1 . ' * ' . $num2 . ' = ' .  $num1 * $num2;
+    } elseif ($operation == 'division') {
+        echo $num1 . ' / ' . $num2 . ' = ' .  $num1 / $num2;
+    } else {
+        echo '正しい演算子を指定して下さい';
+    }
+    ?>
+</body>
+
 </html>
